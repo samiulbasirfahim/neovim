9614af04124405c881a8ca7c94238b5cadf2ea9d
SamiulBasirFahim <samiulbasirfahim.rxen@gmail.com>
2024-10-25 05:55:05 +0600

    auto completion added

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -16,6 +16,7 @@ return {
 		end
 
 		M.capabilities = vim.lsp.protocol.make_client_capabilities()
+		M.capabilities = require("cmp_nvim_lsp").default_capabilities(M.capabilities)
 
 		for _, server in ipairs(servers) do
 			lspconfig[server].setup({
@@ -69,5 +70,39 @@ return {
 				},
 			},
 		})
+
+		local signs = { Error = "✘", Warn = "󱡃", Hint = "󱐮", Info = "󱓔" }
+
+		for type, icon in pairs(signs) do
+			local hl = "DiagnosticSign" .. type
+			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+		end
+
+		vim.diagnostic.config({
+			virtual_text = true,
+			signs = true,
+			update_in_insert = true,
+			underline = true,
+			severity_sort = false,
+			float = {
+				border = "single",
+				source = true,
+				header = "",
+				prefix = "",
+			},
+		})
+
+		vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+			border = "single",
+		})
+
+		vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+			border = "single",
+		})
+
+		vim.o.updatetime = 250
+		vim.cmd([[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]])
+		vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
+		vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
 	end,
 }
--- lua/plugins/tools.lua
+++ lua/plugins/tools.lua
@@ -1,7 +1,1 @@
-return {
-    {
-        "windwp/nvim-autopairs",
-        event = "InsertEnter",
-        config = true,
-    },
-}
+return {}
