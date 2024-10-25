3cb5e7dbcdc3d000e75ab596ed7f01cae82b9a65
SamiulBasirFahim <samiulbasirfahim.rxen@gmail.com>
2024-10-25 17:37:26 +0600

    mini.pick keymaps

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -18,17 +18,18 @@ return {
     },
     {
         "echasnovski/mini.files",
-        keys =
-        { {
-            '<leader>e',
-            function()
-                local _ = require("mini.files").close() or require("mini.files").open()
-            end,
-            mode = 'n',
-            desc = '[E]xplorer',
-        }, },
+        keys = {
+            {
+                "<leader>e",
+                function()
+                    local _ = require("mini.files").close() or require("mini.files").open()
+                end,
+                mode = "n",
+                desc = "[E]xplorer",
+            },
+        },
         config = function()
-            require("mini.files").setup {
+            require("mini.files").setup({
                 use_as_default_explorer = true,
                 windows = {
                     max_number = math.huge,
@@ -37,13 +38,111 @@ return {
                     width_nofocus = 20,
                     width_preview = 25,
                 },
-            }
-        end
+            })
+        end,
     },
     {
         "echasnovski/mini.surround",
         config = function()
             require("mini.surround").setup()
+        end,
+    },
+    ---@type LazyPluginSpec
+    {
+        "echasnovski/mini.pick",
+        dependencies = { "echasnovski/mini.extra" },
+        config = function()
+            local minipick = require("mini.pick")
+            local miniextra = require("mini.extra")
+            -- local minivisits = require("mini.visits")
+            local builtin = minipick.builtin
+
+            miniextra.setup()
+
+            minipick.setup({
+                mappings = {
+                    move_down = "<C-j>",
+                    move_start = "<C-g>",
+                    move_up = "<C-k>",
+                },
+            })
+
+            local keymaps = {
+                {
+                    mode = "n",
+                    key = "<leader>ff",
+                    action = function()
+                        builtin.files()
+                    end,
+                    desc = "Find [f]iles",
+                },
+                {
+                    mode = "n",
+                    key = "<leader>fr",
+                    action = function()
+                        builtin.resume()
+                    end,
+                    desc = "[R]esume finding",
+                },
+                {
+                    mode = "n",
+                    key = "<leader>fl",
+                    action = function()
+                        builtin.grep_live()
+                    end,
+                    desc = "Live [g]rep",
+                },
+                {
+                    mode = "n",
+                    key = "<leader>fb",
+                    action = function()
+                        builtin.buffers()
+                    end,
+                    desc = "Find [b]uffer",
+                },
+                {
+                    mode = "n",
+                    key = "<leader>fh",
+                    action = function()
+                        builtin.help()
+                    end,
+                    desc = "Find [h]elp",
+                },
+                {
+                    mode = "n",
+                    key = "<leader>fw",
+                    action = function()
+                        miniextra.pickers.buf_lines({ scope = "current" })
+                    end,
+                    desc = "Find [w]ord",
+                },
+
+                {
+                    mode = "n",
+                    key = "<leader>fo",
+                    action = function()
+                        miniextra.pickers.oldfiles()
+                    end,
+                    desc = "Find [o]oldfiles",
+                },
+
+                {
+                    mode = "n",
+                    key = "<leader>fc",
+                    action = function()
+                        miniextra.pickers.git_commits()
+                    end,
+                    desc = "Git [c]ommits",
+                },
+            }
+
+            local function map(mode, key, action, desc)
+                vim.keymap.set(mode, key, action, { noremap = true, silent = true, desc = desc })
+            end
+
+            for _, keymap in ipairs(keymaps) do
+                map(keymap.mode, keymap.key, keymap.action, keymap.desc)
+            end
         end,
     },
 }
