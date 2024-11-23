021cba16a0acbb8f24e56f5ff2757ebc1f2733f1
Samiul Basir Fahim <samiulbasirfahim.rxen@gmail.com>
2024-11-23 17:16:40 +0600

    Fixed cd into arg dir issue

--- init.lua
+++ init.lua
@@ -1,60 +1,48 @@
 require(".opts")
--- require(".autocmd")
 require(".keymaps")
+
+if vim.fn.argc() == 1 then
+	vim.api.nvim_create_autocmd("VimEnter", {
+		pattern = "*",
+		callback = function()
+			vim.api.nvim_set_current_dir(vim.fn.expand("%:p:h"))
+		end
+	})
+end
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-    install = { colorscheme = { "gruvbox-material" } },
-    spec = { { import = "plugins" } },
-    ui = { border = "single" },
-    checker = { enabled = true, notify = false },
-    change_detection = { notify = false },
+	spec = {
+		{ import = "plugins" },
+		{ import = "plugins.lsp" },
+	},
 
-    performance = {
-        cache = { enabled = true },
-        reset_packpath = true,
-        rtp = {
-            disabled_plugins = {
-                "2html_plugin",
-                "tohtml",
-                "getscript",
-                "getscriptPlugin",
-                "gzip",
-                "logipat",
-                "netrw",
-                "netrwPlugin",
-                "netrwSettings",
-                "netrwFileHandlers",
-                "matchit",
-                "tar",
-                "tarPlugin",
-                "rrhelper",
-                "spellfile_plugin",
-                "vimball",
-                "vimballPlugin",
-                "zip",
-                "zipPlugin",
-                "tutor",
-                "rplugin",
-                "syntax",
-                "synmenu",
-                "optwin",
-                "compiler",
-                "bugreport",
-                "ftplugin",
-            },
-        },
-    },
+	-- ui config
+	ui = {
+		border = "single",
+		size = {
+			width = 0.85,
+			height = 0.75,
+		},
+	},
+
+	checker = {
+		enabled = true,
+		notify = false,
+	},
+	change_detection = {
+		notify = false,
+	},
 })
--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -24,25 +24,25 @@ return {
             })
         end,
     },
-    -- {
-    --     "echasnovski/mini.files",
-    --     config = function()
-    --         require("mini.files").setup({
-    --             use_as_default_explorer = true,
-    --             windows = {
-    --                 max_number = math.huge,
-    --                 preview = false,
-    --                 width_focus = 20,
-    --                 width_nofocus = 20,
-    --                 width_preview = 20,
-    --             },
-    --             mappings = {
-    --                 mark_goto = "@",
-    --                 reveal_cwd = "'",
-    --             },
-    --         })
-    --     end,
-    -- },
+    {
+        "echasnovski/mini.files",
+        config = function()
+            require("mini.files").setup({
+                use_as_default_explorer = true,
+                windows = {
+                    max_number = math.huge,
+                    preview = false,
+                    width_focus = 20,
+                    width_nofocus = 20,
+                    width_preview = 20,
+                },
+                mappings = {
+                    mark_goto = "@",
+                    reveal_cwd = "'",
+                },
+            })
+        end,
+    },
     {
         "echasnovski/mini.surround",
         config = function()
