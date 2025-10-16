2545b4b5f3efb04054fb8e58a7941231bde6198c
Samiul Basir Fahim <samiulbasirfahim.rxen@gmail.com>
2025-10-17 03:56:24 +0600

    added copilot support

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -1,13 +1,38 @@
-return {
-	"yetone/avante.nvim",
-	build = "make",
-	event = "VeryLazy",
-	version = false,
-	opts = {
-		provider = "gemini",
-		gemini = {
-			model = "gemini-2.5-pro",
-		},
+-- return {
+-- 	"yetone/avante.nvim",
+-- 	build = "make",
+-- 	event = "VeryLazy",
+-- 	version = false,
+-- 	opts = {
+-- 		provider = "gemini",
+-- 		gemini = {
+-- 			model = "gemini-2.5-pro",
+-- 		},
+--
+-- 	},
+-- }
 
+return {
+	{
+		"zbirenbaum/copilot.lua",
+		cmd = "Copilot",
+		event = "InsertEnter",
+		config = function()
+			require("copilot").setup({
+				panel = { enabled = true },
+				suggestion = { auto_trigger = true },
+				filetypes = { markdown = true },
+			})
+		end,
+	},
+
+	{
+		"CopilotC-Nvim/CopilotChat.nvim",
+		dependencies = {
+			{ "nvim-lua/plenary.nvim", branch = "master" },
+		},
+		build = "make tiktoken",
+		opts = {
+		},
 	},
 }
